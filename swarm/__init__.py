e487d5fee990ff78d1fb47d5eff9d4aaaf1c411e
samDobsonDev <sam.dobsonn@gmail.com>
2024-11-09 19:05:21 +0000

    rename

--- swarm/__init__.py
+++ swarm/__init__.py
@@ -1,4 +1,4 @@
-from .core import Swarm, Agent, Response, run_demo_loop
+from .multiagent import Swarm, Agent, Response, run_demo_loop
 
 # This specifies the things that other pieces of code can import via the "swarm" package
 __all__ = ["Swarm", "Agent", "Response", "run_demo_loop"]
