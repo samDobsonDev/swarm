58ae8428be7ea2e52ed9e3a04c322ce0145dcaa0
samDobsonDev <sam.dobsonn@gmail.com>
2024-11-10 19:02:43 +0000

    BAML!

---
+++ swarm/baml.py
@@ -0,0 +1,24 @@
+from baml_client.sync_client import b
+from baml_client.types import Resume
+
+resume_text = """
+      John Doe
+
+      Education
+      - University of California, Berkeley
+        - B.S. in Computer Science
+        - 2020
+
+      Skills
+      - Python
+      - Java
+      - C++
+    """
+
+def example(raw_resume: str) -> Resume: 
+  response = b.ExtractResume(raw_resume)
+  return response
+
+if __name__ == "__main__":
+    resume = example(resume_text)
+    print(resume)
