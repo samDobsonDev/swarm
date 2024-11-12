0d9646020e865c27a4ae75100b67dffd0dbbe047
samDobsonDev <sam.dobsonn@gmail.com>
2024-11-12 14:44:14 +0000

    visualize

--- swarm/visualize.py
+++ swarm/visualize.py
@@ -1,17 +1,34 @@
-import random
 from graphviz import Digraph
 from examples.airline.configs.agents import *
 from swarm.multiagent import Agent
 
 def get_unique_color():
-    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    # Define a list of bold, easily distinguishable colors
+    colors = [
+        "#FF0000",  # Red
+        "#33FF57",  # Green
+        "#3357FF",  # Blue
+        "#FF33A1",  # Pink
+        "#FF8C33",  # Orange
+        "#33FFF5",  # Cyan
+        "#8D33FF",  # Purple
+        "#FFD433",  # Yellow
+        "#33FF8C",  # Lime
+        "#FF3333"   # Bright Red
+    ]
+    # Use a static variable to keep track of the current color index
+    if not hasattr(get_unique_color, "index"):
+        get_unique_color.index = 0
+    # Get the current color and update the index
+    color = colors[get_unique_color.index]
+    get_unique_color.index = (get_unique_color.index + 1) % len(colors)
+    return color
 
 def visualize_agents(initial_agent: Agent):
     dot = Digraph(graph_attr={
         'rankdir': 'LR',
         'splines': 'ortho',
         'nodesep': '0.5',
-        'concentrate': 'true'
     })
     agents = {initial_agent.name: initial_agent}
     agent_queue = [initial_agent]
